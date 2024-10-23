13b1dc10a59bee04390a3edd630fe74aeebe4b81
pretzil-regine-alic <famie.project@gmail.com>
2024-10-23 15:12:10 +0800

    another update 1

--- lib/services/config.dart
+++ lib/services/config.dart
@@ -1,4 +1,4 @@
 // filename: services/config.dart
 class Config {
-  static const String baseUrl = 'https://famie-backend-5acd1032b9ba.herokuapp.com/';
+  static const String baseUrl = 'https://famie-backend-5acd1032b9ba.herokuapp.com';
 }
